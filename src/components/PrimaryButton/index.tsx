5097106a012458e828584a454b76a9119f3da6d5
José Lucas Santos Barbosa <jlsbarbosa@MacBookAir-ZeLucas.local>
2025-09-08 18:49:01 -0300

    refactor: bring more consistence with typing of typescript

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -2,7 +2,11 @@ import { TouchableOpacity, Text } from 'react-native';
 
 import { styles } from './styles'
 
-export function PrimaryButton(props) {
+type PrimaryButtonProps = {
+  title: string
+}
+
+export function PrimaryButton(props: PrimaryButtonProps) {
   return (
     <TouchableOpacity style={styles.container} activeOpacity={0.8}>
       <Text style={styles.title}>{props.title || '*** New Button ***'}</Text>
