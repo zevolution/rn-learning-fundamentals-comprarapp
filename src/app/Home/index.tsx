e51ce7a5498c998c0d6a91e48109eda937b4e418
José Lucas Santos Barbosa <jlsbarbosa@MacBookAir-ZeLucas.local>
2025-09-08 17:46:57 -0300

    feat: add application logo and their assets

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native';
+import { View, Image } from 'react-native';
 
 import { styles } from './styles'
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Image source={require("../../assets/logo.png")}/>
     </View>
   );
 }
