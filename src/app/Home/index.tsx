f630e0c5be743a7b065f75dda3f186c6b320eaf6
José Lucas Santos Barbosa <jlsbarbosa@MacBookAir-ZeLucas.local>
2025-09-08 17:44:17 -0300

    refactor: split App file in 2 files that represents component and styling of Home screen

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import { styles } from './styles'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+
